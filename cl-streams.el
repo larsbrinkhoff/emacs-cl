eb6a32f3c6b4f8e5896d9bf023a3bb87b86625c7
Lars Brinkhoff <lars@nocrew.org>
2003-09-29 20:24:14 +0000

    add with-input-from-string and support for stream designators

--- cl-streams.el
+++ cl-streams.el
@@ -4,6 +4,12 @@
 ;;;;
 ;;;; This file implements operators in chapter 21, Streams.
 
+(defvar *standard-input*)
+
+(defvar *standard-output*)
+
+(defvar *terminal-io*)
+
 (defstruct (stream (:predicate streamp) (:copier nil))
   filename
   content
@@ -24,24 +30,42 @@
 	(unread-char char stream)
 	(return-from peek-char char))))))
 
-(defun* cl:read-char (&optional stream (eof-error-p t) eof-value recursive-p)
-  (let ((ch (funcall (stream-read-fn stream) stream)))
+(defun resolve-input-stream-designator (designator)
+  (case designator
+    ((nil)	*standard-input*)
+    ((t)	*terminal-io*)
+    (t		designator)))
+
+(defun resolve-output-stream-designator (designator)
+  (case designator
+    ((nil)	*standard-output*)
+    ((t)	*terminal-io*)
+    (t		designator)))
+
+(defun* cl:read-char (&optional stream-designator (eof-error-p t)
+				eof-value recursive-p)
+  (let* ((stream (resolve-input-stream-designator stream-designator))
+	 (ch (funcall (stream-read-fn stream) stream)))
     (if (eq ch :eof)
 	(if eof-error-p
 	    (error "end of file")
 	    eof-value)
 	ch)))
 
-(defun unread-char (char &optional stream)
-  (when (> (stream-index stream) 0)
-    (decf (stream-index stream))))
+(defun unread-char (char &optional stream-designator)
+  (let ((stream (resolve-input-stream-designator stream-designator)))
+    (when (> (stream-index stream) 0)
+      (decf (stream-index stream)))))
 
-(defun cl:write-char (char &optional stream)
-  (funcall (stream-write-fn stream) char stream)
-  char)
+(defun cl:write-char (char &optional stream-designator)
+  (let ((stream (resolve-output-stream-designator stream-designator)))
+    (funcall (stream-write-fn stream) char stream)
+    char))
 
-(defun* read-line (&optional stream (eof-error-p t) eof-value recursive-p)
-  (let ((line ""))
+(defun* read-line (&optional stream-designator (eof-error-p t)
+			     eof-value recursive-p)
+  (let ((stream (resolve-input-stream-designator stream-designator))
+	(line ""))
     (loop
      (let ((char (cl:read-char stream eof-error-p eof-value recursive-p)))
        (cond
@@ -52,16 +76,23 @@
 	  (return-from read-line (values line nil))))
        (setq line (concat line (list char)))))))
 
-(defun* write-string (string &optional stream &key (start 0) end)
-  (message "length %d" (length string))
-  (do ((i 0 (1+ i)))
+(defun* write-string (string &optional stream-designator &key (start 0) end)
+  (do ((stream (resolve-output-stream-designator stream-designator))
+       (i 0 (1+ i)))
       ((= i (length string)) string)
     (cl:write-char (aref string i) stream)))
 
-(defun* write-line (string &optional stream &key (start 0) end)
-  (write-string string stream :start start :end end)
-  (cl:write-char 10 stream)
-  string)
+(defun* write-line (string &optional stream-designator &key (start 0) end)
+  (let ((stream (resolve-output-stream-designator stream-designator)))
+    (write-string string stream :start start :end end)
+    (cl:write-char 10 stream)
+    string))
+
+;;; TODO: read-sequence
+
+;;; TODO: write-sequence
+
+;;; TODO: file-length
 
 (defun file-position (stream)
   (stream-index stream))
@@ -89,6 +120,8 @@
 			     (goto-char (incf (stream-index stream)))
 			     (insert char)))))
 
+;;; TODO: stream-external-format
+
 (defmacro* with-open-file ((stream filespec &rest options) &body body)
   `(with-open-stream (,stream (open ,filespec ,@options))
      ,@body))
@@ -107,6 +140,34 @@
     (unwind-protect
 	 (progn ,@body)
       (close ,var))))
+
+;;; TODO: listen
+
+;;; TODO: clear-input
+
+;;; TODO: finish-output, force-output, clear-output
+
+;;; TODO: y-or-n-p, yes-or-no-p
+
+;;; TODO: make-synonym-stream
+
+;;; TODO: synonym-stream-symbol
+
+;;; TODO: broadcast-stream-streams
+
+;;; TODO: make-broadcast-stream
+
+;;; TODO: make-two-way-stream
+
+;;; TODO: two-way-stream-input-stream, two-way-stream-output-stream
+
+;;; TODO: echo-stream-input-stream, echo-stream-output-stream
+
+;;; TODO: make-echo-stream
+
+;;; TODO: concatenated-stream-streams
+
+;;; TODO: make-concatenated-stream
 
 (defun get-output-stream-string (stream)
   (stream-content stream))
@@ -137,3 +198,12 @@
 	         (lambda (char stream)
 		   (setf (stream-content stream)
 			 (concat (stream-content stream) (list char))))))
+
+(defmacro* with-input-from-string ((var string &key index (start 0) end)
+				   &body body)
+  `(with-open-stream (,var (make-string-input-stream ,string ,start ,end))
+     ,@body))
+
+;;; TODO: with-output-to-string (needs strings with fill pointers)
+
+;;; TODO: stream-error-stream
