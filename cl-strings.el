1fc42587ffb1becda0017e3fb9d550d068adda21
Lars Brinkhoff <lars@nocrew.org>
2003-09-29 23:21:31 +0000

    some more support for characters and strings

--- cl-strings.el
+++ cl-strings.el
@@ -4,15 +4,48 @@
 ;;;;
 ;;;; This file implements operators in chapter 16, Strings.
 
-(defun* cl:make-string (size &key initial-element element-type)
-  (make-string size (or initial-element 0)))
+(defun simple-string-p (object)
+  (stringp object))
+
+(defun char (string index)
+  (cond
+    ((simple-string-p string)
+     (schar string index))
+    ((cl:stringp string)
+     (schar (aref string 2) index))
+    (t
+     (error "type error"))))
+
+(defun schar (string index)
+  (code-char (aref string index)))
+
+(defun setf-char (string index new)
+  (check-type new 'character)
+  (cond
+    ((simple-string-p string)
+     (setf (schar string index) new))
+    ((cl:stringp string)
+     (setf (schar (aref string 2) index) new))
+    (t
+     (error "type error"))))
+
+(defun setf-schar (string index new)
+  (check-type new 'character)
+  (aset string index (char-code new)))
+
+(defsetf schar setf-schar)
+(defsetf char setf-char)
 
 (defun cl:string (x)
   (cond
-    ((stringp x) x)
-    ((symbolp x) (symbol-name x))
-    ((characterp x) (concat (list x)))
-    (t (error))))
+    ((cl:stringp x)	x)
+    ((symbolp x)	(symbol-name x))
+    ((characterp x)	(cl:make-string 1 :initial-element x))
+    (t			(error))))
 
-(defun char (string index)
-  (aref string index))
+(defun cl:stringp (object)
+  (or (simple-string-p object)
+      (and (vectorp object) (eq (aref object 0) 'string))))
+
+(defun* cl:make-string (size &key initial-element element-type)
+  (make-string size (if initial-element (char-code initial-element) 0)))
