e28822f17bb103506f7e0152d15919bef67a6135
Lars Brinkhoff <lars@nocrew.org>
2003-10-16 16:49:29 +0000

    add dotimes

--- cl-iteration.el
+++ cl-iteration.el
@@ -5,6 +5,17 @@
 
 (IN-PACKAGE "EMACS-CL")
 
+;;; TODO: (cl:defmacro DO ...)
+
+;;; TODO: (cl:defmacro DO* ...)
+
+(cl:defmacro DOTIMES ((var count &optional result) &body body)
+  `(DO ((,var 0 (1+ ,var))
+	(,end ,count))
+       ((EQL ,var ,end)
+	,result)
+     ,@body))
+
 (cl:defmacro DOLIST ((var list &optional result) &body body)
   `(PROGN
     (MAPC (LAMBDA (,var) ,@body) ,list)
