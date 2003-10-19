1f879ca38e854fbf8bc0be5b50fe96e6b43dbbeb
Lars Brinkhoff <lars@nocrew.org>
2003-10-19 07:00:50 +0000

    add do and do* macros

--- cl-iteration.el
+++ cl-iteration.el
@@ -5,9 +5,41 @@
 
 (IN-PACKAGE "EMACS-CL")
 
-;;; TODO: (cl:defmacro DO ...)
+(defun var-inits (vars)
+  (mapcar (lambda (var)
+	    (if (symbolp var)
+		var
+		`(,(first var) ,(second var))))
+	  vars))
 
-;;; TODO: (cl:defmacro DO* ...)
+(defun var-steps (vars)
+  (mappend (lambda (var)
+	     (print var)
+	     (when (and (consp var) (= (length var) 3))
+	       `(,(first var) ,(third var))))
+	   vars))
+
+(cl:defmacro DO (vars (test &rest result) &body body)
+  (with-gensyms (block start)
+    `(LET* ,(var-inits vars)
+       (BLOCK ,block
+	 (TAGBODY
+	   ,start
+	   (WHEN ,test (RETURN-FROM ,block (PROGN ,@result)))
+	   ,@body
+	   (PSETQ ,@(var-steps vars))
+	   (GO ,start))))))
+
+(cl:defmacro DO* (vars (test &rest result) &body body)
+  (with-gensyms (block start)
+    `(LET* ,(var-inits vars)
+       (BLOCK ,block
+	 (TAGBODY
+	   ,start
+	   (WHEN ,test (RETURN-FROM ,block (PROGN ,@result)))
+	   ,@body
+	   (SETQ ,@(var-steps vars))
+	   (GO ,start))))))
 
 (cl:defmacro DOTIMES ((var count &optional result) &body body)
   `(DO ((,var 0 (1+ ,var))
@@ -17,6 +49,12 @@
      ,@body))
 
 (cl:defmacro DOLIST ((var list &optional result) &body body)
+;   (with-gensyms (x)
+;     `(DO* ((,x ,list (CDR ,x)))
+;           ((NULL ,x)
+; 	   ,result)
+;        (LET ((,var (CAR ,x)))
+; 	 ,@body))))
   `(PROGN
     (MAPC (LAMBDA (,var) ,@body) ,list)
     ,result))
