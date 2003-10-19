c07a83a3a896717c7b4c0222deff46f168a3a368
Lars Brinkhoff <lars@nocrew.org>
2003-10-19 06:54:49 +0000

    add sleep, time, internal-time-units-per-second, get-internal-real-time

--- cl-environment.el
+++ cl-environment.el
@@ -5,6 +5,37 @@
 
 (IN-PACKAGE "EMACS-CL")
 
+(defun SLEEP (seconds)
+  (sit-for (if (or (integerp seconds) (floatp seconds))
+	       seconds
+	       (FLOAT seconds))
+	   0 t)
+  ;; TODO: if sit-for didn't return t, sleep some more.
+  nil)
+
+(cl:defmacro TIME (form)
+  (with-gensyms (start val end)
+    `(LET* ((,start (GET-INTERNAL-REAL-TIME))
+	    (,val (MULTIPLE-VALUE-LIST ,form))
+	    (,end (GET-INTERNAL-REAL-TIME)))
+       (PRINC "\nElapsed real time: ")
+       (PRIN1 (,(INTERN "*" "CL")
+	       (,(INTERN "-" "CL") ,end ,start)
+	       ,(/ 1.0 INTERNAL-TIME-UNITS-PER-SECOND)))
+       (PRINC " seconds")
+       (VALUES-LIST ,val))))
+
+(DEFCONSTANT INTERNAL-TIME-UNITS-PER-SECOND 1000000)
+
+(defun GET-INTERNAL-REAL-TIME ()
+  (let* ((time (current-time))
+	 (high (first time))
+	 (low (second time))
+	 (microsec (third time)))
+    (binary+ (binary* (binary+ (binary* high 65536) low) 1000000) microsec)))
+
+;;; (defun GET-INTERNAL-RUN-TIME ())
+
 (defvar cl:* nil)
 (defvar ** nil)
 (defvar ***)
