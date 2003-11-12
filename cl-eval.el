0302cc9ea75e2b593cd3f9729060a0bd8a689a11
Lars Brinkhoff <lars@nocrew.org>
2003-11-12 07:39:27 +0000

    always allocate new storage for new bindings

--- cl-eval.el
+++ cl-eval.el
@@ -84,44 +84,47 @@
       (dolist (form body lastval)
 	(setq lastval (eval-with-env form new-env))))))
 
+(defun lexical-binding-variables (bindings)
+  (mappend (lambda (binding)
+	     (let ((var (if (symbolp binding)
+			    binding
+			    (car binding))))
+	       (unless (eq (nth-value 0 (variable-information var env))
+			   :special)
+		 (list var))))
+	   bindings))
+
 ;;; TODO: let* bindings shouldn't be evaluated in an environment where
 ;;; succeeding bindings exist.
 (defun eval-let (bindings forms env old-env)
-  (let* ((vars (mappend (lambda (binding)
-			  (let ((var (if (symbolp binding)
-					 binding
-					 (car binding))))
-			    (unless (eq (nth-value 0 (variable-information var env))
-					:special)
-			      (list var))))
-			bindings))
-	 (new-env (if vars (augment-environment env :variable vars) env))
-	 (lastval nil)
-	 (oldvals nil))
-    (dolist (binding bindings)
-      (multiple-value-bind (var val) (if (symbolp binding)
-					 (values binding nil)
-					 (values (first binding)
-						 (second binding)))
-	(if (member var vars)
-	    (setf (lexical-value var new-env)
-		  (eval-with-env val (or old-env new-env)))
-	    (progn
-	      (push (symbol-value var) oldvals)
-	      (setf (symbol-value var)
-		    (eval-with-env val (or old-env new-env)))))))
-    (unwind-protect
-	 (MULTIPLE-VALUE-BIND (body declarations) (parse-body forms)
-	   (dolist (form body lastval)
-	     (setq lastval (eval-with-env form new-env))))
-      (setq oldvals (nreverse oldvals))
+  (MULTIPLE-VALUE-BIND (body declarations) (parse-body forms)
+    (let* ((vars (lexical-binding-variables bindings))
+	   (new-env (if vars (augment-environment env :variable vars) env))
+	   (lastval nil)
+	   (oldvals nil))
       (dolist (binding bindings)
 	(multiple-value-bind (var val) (if (symbolp binding)
 					   (values binding nil)
 					   (values (first binding)
 						   (second binding)))
-	  (unless (member var vars)
-	    (setf (symbol-value var) (pop oldvals))))))))
+	  (if (member var vars)
+	      (setf (lexical-value var new-env)
+		    (eval-with-env val (or old-env new-env)))
+	      (progn
+		(push (symbol-value var) oldvals)
+		(setf (symbol-value var)
+		      (eval-with-env val (or old-env new-env)))))))
+      (unwind-protect
+	   (dolist (form body lastval)
+	     (setq lastval (eval-with-env form new-env)))
+	(setq oldvals (nreverse oldvals))
+	(dolist (binding bindings)
+	  (multiple-value-bind (var val) (if (symbolp binding)
+					     (values binding nil)
+					     (values (first binding)
+						     (second binding)))
+	    (unless (member var vars)
+	      (setf (symbol-value var) (pop oldvals)))))))))
 
 (define-special-operator LET (bindings &rest forms) env
   (eval-let bindings forms env env))
@@ -210,15 +213,19 @@
 	 (no-tag 0.0))
     (while exe
       (let ((form (first exe)))
-	(if (go-tag-p form)
-	    (setq exe (rest exe))
-	    (let ((tag (catch catch-tag
-			 (eval-with-env form new-env)
-			 no-tag)))
-	      (if (eq tag no-tag)
-		  (setq exe (rest exe))
-		  (setq exe (member tag forms)))))))
-    nil))
+	(cond
+	  ((go-tag-p form)
+	   (setq exe (rest exe)))
+	  ((consp form)
+	   (let ((tag (catch catch-tag
+			(eval-with-env form new-env)
+			no-tag)))
+	     (if (eq tag no-tag)
+		 (setq exe (rest exe))
+		 (setq exe (member tag forms)))))
+	  (t
+	   (ERROR "Syntax error: ~S in tagbody is neither a go tag ~
+		   nor a compound expression" form)))))))
 
 (define-special-operator THE (type form) env
   (eval-with-env form env))
@@ -248,14 +255,16 @@
     nil))
 
 (defun lexical-value (var env)
-  (cdr (assoc var (aref env 3))))
+  (cdr (assq var (aref env 3))))
 
 (defsetf lexical-value (var env) (val)
-  `(let ((cons (assoc ,var (aref ,env 3))))
-     (if cons
-	 (setf (cdr cons) ,val)
-	 (progn (aset ,env 3 (acons ,var ,val (aref ,env 3)))
-		,val))))
+  `(setf (cdr (assq ,var (aref ,env 3))) ,val))
+
+;   `(let ((cons (assoc ,var (aref ,env 3))))
+;      (if cons
+; 	 (setf (cdr cons) ,val)
+; 	 (progn (aset ,env 3 (acons ,var ,val (aref ,env 3)))
+; 		,val))))
 
 (defun function-information (fn &optional env)
   (unless env
@@ -272,7 +281,7 @@
     nil))
 
 (defun lexical-function (name env)
-  (cdr (assoc name (aref env 6))))
+  (cdr-safe (assoc name (aref env 6))))
 
 (defsetf lexical-function (name env) (fn)
   `(let ((cons (assoc ,name (aref ,env 6))))
@@ -296,6 +305,7 @@
     (setq env *global-environment*))
   (let ((var-info (aref env 1))
 	(var-local (aref env 2))
+	(var-storage (aref env 3))
 	(fn-info (aref env 4))
 	(fn-local (aref env 5))
 	(block-info (aref env 7))
@@ -307,6 +317,8 @@
 			   symbol-macro
 			   :initial-value var-info))
     (setq var-local (append variable var-local))
+    (dolist (var variable)
+      (push (cons var nil) var-storage))
     (setq fn-info (reduce (lambda (env fn) (acons fn :function env))
 			  function
 			  :initial-value fn-info))
@@ -316,7 +328,7 @@
     (setq fn-local (append function fn-local))
     (setq block-info (cons block block-info))
     (setq tagbody-info (cons tagbody tagbody-info))
-  (vector 'environment var-info var-local (aref env 3)
+  (vector 'environment var-info var-local var-storage
 	               fn-info fn-local (aref env 6)
 	               block-info tagbody-info)))
 
